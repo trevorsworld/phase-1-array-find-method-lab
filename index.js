2035f0fe19e2edcee2cf41f213fdcb513d3cf1c0
trevors banu <trevorskarithi@gmail.com>
2024-03-25 13:41:16 +0300

    adds phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(superbowlResults) {
+    const winningYear = superbowlResults.find(result => result.result === "W");
+    return winningYear ? winningYear.year : undefined;
+}
+
+const superbowlResults = [
+    { year: 2020, result: "L" },
+    { year: 2021, result: "W" },
+    { year: 2022, result: "L" }
+];
+
+console.log(superbowlWin(superbowlResults));
